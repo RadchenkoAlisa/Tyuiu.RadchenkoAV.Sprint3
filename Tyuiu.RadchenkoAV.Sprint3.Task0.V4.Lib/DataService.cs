26a80cd2f46d2b76ee5ac91cb1d17ad92c3a60fd
alisa69 <aliska132007@gmail.ru>
2025-10-31 02:25:28 +0500

    Add project files.

---
+++ Tyuiu.RadchenkoAV.Sprint3.Task0.V4.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.RadchenkoAV.Sprint3.Task0.V4.Lib
+
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint3.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.RadchenkoAV.Sprint3.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
