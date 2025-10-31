a83fa560bae2271cad0e5fa1a7ca48d0f0cf2c49
alisa69 <aliska132007@gmail.ru>
2025-10-31 10:34:07 +0500

    Task2

---
+++ Tyuiu.RadchenkoAV.Sprint3.Task2.V13.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.RadchenkoAV.Sprint3.Task2.V13.Lib
+{
+    public class DataService : ISprint3Task2V13
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double SumSeries = 0;
+            do
+            {
+                SumSeries = SumSeries + (((Math.Pow(value, startValue)) + (2 / (startValue + 1))) * (Math.Sin(value)));
+                startValue++;
+
+            } while (startValue <= stopValue);
+            return Math.Round(SumSeries, 3);
+        }
+    }
+}
