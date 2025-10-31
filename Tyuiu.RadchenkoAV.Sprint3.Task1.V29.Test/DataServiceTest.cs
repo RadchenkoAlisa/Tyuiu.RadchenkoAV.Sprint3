2ff049081cf4fc978a399cd577b14e79d3a743c2
alisa69 <aliska132007@gmail.ru>
2025-10-31 10:27:22 +0500

    Task1

---
+++ Tyuiu.RadchenkoAV.Sprint3.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.RadchenkoAV.Sprint3.Task1.V29.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint3.Task1.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMultiplySeriesTest()
+        {
+            DataService ds = new DataService();
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 11;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 13051.34;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
