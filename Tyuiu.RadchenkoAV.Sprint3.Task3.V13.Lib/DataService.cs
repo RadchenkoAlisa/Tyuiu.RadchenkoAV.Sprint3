4a8a33d656390c68d7818d9922307f666ae4f836
alisa69 <aliska132007@gmail.ru>
2025-10-31 10:45:43 +0500

    Task3

---
+++ Tyuiu.RadchenkoAV.Sprint3.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.RadchenkoAV.Sprint3.Task3.V13.Lib
+{
+    public class DataService : ISprint3Task3V13
+    {
+        public int ConvertStringToInt(string value)
+        {
+
+            string a = "";
+            foreach (char c in value)
+            {
+                if (Char.IsDigit(c))
+                {
+                    a += c;
+                }
+            }
+
+            return Int32.Parse(a);
+        }
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint3.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.RadchenkoAV.Sprint3.Task3.V13.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint3.Task3.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidConvertStringToInt()
+        {
+            DataService ds = new DataService();
+            string value = "?sd!! 5gh. s!";
+            double res = ds.ConvertStringToInt(value);
+            int wait = 5;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
